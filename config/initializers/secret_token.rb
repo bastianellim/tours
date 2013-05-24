dda948127eca7af27358a904b52d345a062b0901
Manuele Bastianelli <bastianellim@gmail.com>
2013-05-24 02:23:42 +0200

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tours::Application.config.secret_token = '7bd9ce90a3b5ca8e09c4c32b1eb847ef97c256ba8b8c32ac27d93b03c1d516e162134f49bd8e1804b539d22b8901296820505d5d69a71267c40218b638da4fa2'
